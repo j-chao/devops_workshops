e19eaed875f911410967547543a6427789d9d110
Justin Chao <justin.chao@optum.com>
2019-05-22 10:05:40 -0400

    got plugins to install!

--- provisioning/jenkins_provision.sh
+++ provisioning/jenkins_provision.sh
@@ -8,7 +8,7 @@ wget -qq -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
 # Update package database
-sudo apt-get -qq update
+sudo apt-get -qq update --fix-missing
 
 # Install Jenkins
 sudo apt-get -qq install default-jre=2:1.8-56ubuntu2
@@ -33,7 +33,6 @@ instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
 
 instance.setSystemMessage("Jenkins Training Environment for DevOps Workshop.")
 instance.disableSecurity()
-
 instance.save()
 
 urlConfig = JenkinsLocationConfiguration.get()
@@ -45,13 +44,36 @@ for (AdministrativeMonitor am : ams) {
   am.disable(true) 
 }
 
-instance.getUpdateCenter().getPlugin("git").deploy()
-instance.getUpdateCenter().getPlugin("workflow-aggregator").deploy()
 
-instance.restart()
+def installed = false
+def initialized = false
+def pluginParameter="git workflow-aggregator"
+def plugins = pluginParameter.split()
+def pm = instance.getPluginManager()
+def uc = instance.getUpdateCenter()
+
+plugins.each {
+  if (!pm.getPlugin(it)) {
+    if (!initialized) {
+      uc.updateAllSites()
+      initialized = true
+    }
+    def plugin = uc.getPlugin(it)
+    if (plugin) {
+    	def installFuture = plugin.deploy()
+      while(!installFuture.isDone()) {
+        sleep(3000)
+      }
+      installed = true
+    }
+  }
+}
+if (installed) {
+  instance.save()
+  instance.restart()
+}
+
 
 EOF
 
-
-sudo systemctl restart jenkins
 
